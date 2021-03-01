81ecf314905e29ee90768997f4d109b98cdd9012
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2021-03-01 17:12:37 +0100

    Added buttons

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ function Tweet() {
   return (
     <div className="tweet">
       <Avatar />
-      <Message />
-      <Author />
-      Tweet
+      <div className="content">
+        <Author /> <Time />
+        <Message />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
     </div>
   );
 }
@@ -34,5 +41,17 @@ function Author() {
     </span>
   );
 }
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
 
 ReactDOM.render(<Tweet />, document.querySelector("#root"));
